aea15eb3159ba581d92da81f04e20b2ddd8d512c
Sahas-Ananth <sahas_ananth@outlook.com>
2021-02-18 22:57:45 +0530

    Optimization 1 done.
    - Removed scroll and pan controls from the map.
    - Made to make one ajax request for updating table, graph and map
    instead of three.

--- docs/main.js
+++ docs/main.js
@@ -1,150 +1,153 @@
+var jsonDataObject = [];
+
+function RefreshAll() {
+	$.getJSON('https://spreadsheets.google.com/feeds/list/1JvXdYoYRmdPyDNdNzlCEjxApYi0O4UX8D39OOYxgt0o/5/public/full?alt=json', function(data) 
+		{
+			for (var i = 0; i < data.feed.entry.length; ++i) {
+
+				var json_data = {
+						"Timestamp":data.feed.entry[i].gsx$timestamp,
+						"OrderID": data.feed.entry[i].gsx$orderid.$t,
+						"Item": data.feed.entry[i].gsx$item.$t,
+						"Priority": data.feed.entry[i].gsx$priority.$t,
+						"Quantity": data.feed.entry[i].gsx$quantity.$t,
+						"City": data.feed.entry[i].gsx$city.$t,
+						"Longitude": parseFloat(data.feed.entry[i].gsx$longitude.$t),
+						"Latitude": parseFloat(data.feed.entry[i].gsx$latitude.$t),
+						"Dispatched": data.feed.entry[i].gsx$dispatched.$t,
+						"Shipped": data.feed.entry[i].gsx$shipped.$t,
+						"Order_Date_and_Time": data.feed.entry[i].gsx$orderdateandtime,
+						"Dispatch_Date_and_Time": data.feed.entry[i].gsx$dispatchdateandtime,
+						"Shipped_Date_and_Time": data.feed.entry[i].gsx$shippeddateandtime,
+						"TimeTaken": parseFloat(data.feed.entry[i].gsx$timetaken.$t)
+				};
+				jsonDataObject.push(json_data);
+				TableRefresh();
+				ChartRefresh();
+				MapRefresh();
+			}
+		}
+	)
+}
+
+
 function TableRefresh() {
-    refreshTable();
-    setInterval(refreshTable, 1000);
+	refreshTable();
+	setInterval(refreshTable, 1000);
 }
 
 function ChartRefresh() {
-    refreshChart();
-    setInterval(refreshChart, 5000);
+	refreshChart();
+	setInterval(refreshChart, 5000);
 }
 
 function MapRefresh() {
-    refreshMap();
-    setInterval(refreshMap, 5000);
+	refreshMap();
+	setInterval(refreshMap, 5000);
 }
 
 function refreshTable() {
-    $.getJSON('https://spreadsheets.google.com/feeds/list/1JvXdYoYRmdPyDNdNzlCEjxApYi0O4UX8D39OOYxgt0o/5/public/full?alt=json', function(data) 
-    {   
-        var trHTML = '';
-        for (var i = 0; i < data.feed.entry.length; ++i) {
-            trHTML += '<tr><td>' + data.feed.entry[i].gsx$orderid.$t + 
-                        '</td><td>' + data.feed.entry[i].gsx$item.$t + 
-                        '</td><td>' + data.feed.entry[i].gsx$priority.$t + 
-                        '</td><td>' + data.feed.entry[i].gsx$quantity.$t + 
-                        '</td><td>'  + data.feed.entry[i].gsx$city.$t + 
-                        '</td></tr>';
-        }
-        console.log(trHTML);
-        $('#tableContent').html(trHTML);
-        var trHTML = '';
-    }
-    );
+	var trHTML = '';
+	for (var j = 0; j < jsonDataObject.length; ++j) {
+			trHTML += 	'<tr><td>' + jsonDataObject[j].OrderID + 
+						'</td><td>' + jsonDataObject[j].Item + 
+						'</td><td>' + jsonDataObject[j].Priority + 
+						'</td><td>' + jsonDataObject[j].Quantity + 
+						'</td><td>'  + jsonDataObject[j].City + 
+						'</td></tr>';
+	}
+	console.log(trHTML);
+	$('#tableContent').html(trHTML);
+	var trHTML = '';
 }
 
 function refreshMap(){
-    var container = L.DomUtil.get('map');
+	var container = L.DomUtil.get('map');
 
-      if(container != null){
-    container._leaflet_id = null;
-    }
-    var map=L.map('map').setView([20.5937, 78.9629], 5);
-    var jsonDataObject =[];
+	if(container != null){
+		container._leaflet_id = null;
+	}
+	var map=L.map('map', {zoomControl: false}).setView([23.0000, 81.0000], 5);
 
-    $.getJSON('https://spreadsheets.google.com/feeds/list/1JvXdYoYRmdPyDNdNzlCEjxApYi0O4UX8D39OOYxgt0o/5/public/full?alt=json', function(data) {
-    for (var i = 0; i < data.feed.entry.length; ++i) {
+	map.touchZoom.disable();
+	map.doubleClickZoom.disable();
+	map.scrollWheelZoom.disable();
+	map.boxZoom.disable();
+	map.keyboard.disable();
 
-        var json_data = {
-            "City": data.feed.entry[i].gsx$city.$t,
-            "OrderID" : data.feed.entry[i].gsx$orderid.$t,
-            "Item" : data.feed.entry[i].gsx$item.$t,
-            "Latitude": parseFloat(data.feed.entry[i].gsx$latitude.$t),
-            "Longitude": parseFloat(data.feed.entry[i].gsx$longitude.$t),
-            "Dispatched":data.feed.entry[i].gsx$dispatched.$t,
-            "Shipped":data.feed.entry[i].gsx$shipped.$t
-        };
-        jsonDataObject.push(json_data);
+	for (var j = 0; j < jsonDataObject.length; j++) {
+			var customColour = "red";
+			var pos = L.latLng(parseFloat(jsonDataObject[j].Latitude), parseFloat(jsonDataObject[j].Longitude))
 
-        for (var j = 0; j < jsonDataObject.length; j++) {
+			if (jsonDataObject[j].Dispatched == "Yes" && jsonDataObject[j].Shipped == ""){
+					customColour = "orange";
+			}
+			else if ((jsonDataObject[j].Dispatched == "Yes" && jsonDataObject[j].Shipped == "Yes")){
+					customColour = "green";
+			}
+			
+			var customMarker = L.AwesomeMarkers.icon({
+					markerColor: customColour
+			});
 
-            var customColour = "red";
+			var marker = L.marker(pos, {icon: customMarker});
+			marker.bindPopup(jsonDataObject[j].City, {
+							autoClose: false
+			});
+			map.addLayer(marker);
+			marker.on('click', onClick_Marker)
+			// Attach the corresponding JSON data to your marker:
+			marker.myJsonData =jsonDataObject[j];
 
-            var pos = L.latLng(parseFloat(jsonDataObject[j].Latitude), parseFloat(jsonDataObject[j].Longitude))
+			function onClick_Marker(e) {
+							var marker = e.target;
+							popup = L.popup()
+							.setLatLng(marker.getLatLng())
+							.setContent("Order ID: " + marker.myJsonData.OrderID + " || Item: " +   marker.myJsonData.Item)
+							.openOn(map);
+					}
 
-            if (jsonDataObject[j].Dispatched == "Yes" && jsonDataObject[j].Shipped == ""){
-                customColour = "orange";
-            }
-            else if ((jsonDataObject[j].Dispatched == "Yes" && jsonDataObject[j].Shipped == "Yes")){
-                customColour = "green";
-            }
-            
-            var customMarker = L.AwesomeMarkers.icon({
-                markerColor: customColour
-            });
+			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+							attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			}).addTo(map); 
+							
 
-            var marker = L.marker(pos, {icon: customMarker});
-            marker.bindPopup(jsonDataObject[j].City, {
-                    autoClose: false
-            });
-            map.addLayer(marker);
-            marker.on('click', onClick_Marker)
-            // Attach the corresponding JSON data to your marker:
-            marker.myJsonData =jsonDataObject[j];
-    
-            function onClick_Marker(e) {
-                    var marker = e.target;
-                    popup = L.popup()
-                    .setLatLng(marker.getLatLng())
-                    .setContent("Order ID: " + marker.myJsonData.OrderID + " || Item: " +   marker.myJsonData.Item)
-                    .openOn(map);
-                }
-
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map); 
-                
-
-            }
-        }
-    });
+	}
 }
 
 google.charts.load("current", {packages:['corechart']});
 google.charts.setOnLoadCallback(refreshChart);
 
 function refreshChart(){
-    var jsonDataObject =[];
-    var graph_arr = [['Order ID', 'Time Taken', { role: 'style' }]];
-    var bar_color = [];
-    $.getJSON('https://spreadsheets.google.com/feeds/list/1JvXdYoYRmdPyDNdNzlCEjxApYi0O4UX8D39OOYxgt0o/5/public/full?alt=json', function(data) {
-      for (var i = 0; i < data.feed.entry.length; ++i) {
-        var json_data = {
-          "OderID" : data.feed.entry[i].gsx$orderid.$t,
-          "TimeTaken": parseFloat(data.feed.entry[i].gsx$timetaken.$t),
-          "Priority": data.feed.entry[i].gsx$priority.$t
-          };
-          jsonDataObject.push(json_data);
-      };
-      // Setting color for the coloumns of graph according to priority of items
-      for(var j in jsonDataObject){
-        if(jsonDataObject[j].Priority == 'HP'){
-          var color =  '#FF0000';
-          }
-        else if(jsonDataObject[j].Priority == 'MP'){
-          var color =  '#FFFF00';
-          }
-        else if(jsonDataObject[j].Priority == 'LP'){
-          var color =  '#00FF00';
-          }
-        bar_color.push(color)
-      }
+	var graph_arr = [['Order ID', 'Time Taken', { role: 'style' }]];
+	var bar_color = [];
+	for (var j in jsonDataObject) {
+		if(jsonDataObject[j].Priority == 'HP'){
+			var color =  '#FF0000';
+			}
+		else if(jsonDataObject[j].Priority == 'MP'){
+			var color =  '#FFFF00';
+			}
+		else if(jsonDataObject[j].Priority == 'LP'){
+			var color =  '#00FF00';
+			}
+		bar_color.push(color)
+	}
 
-      // Converting Json Object to JavaScript Array
-      for(var j in jsonDataObject){
-          graph_arr.push([jsonDataObject[j].OderID,jsonDataObject[j].TimeTaken, bar_color[j]]);
-      }
-      var graphArray_Final = google.visualization.arrayToDataTable(graph_arr);
-    
-      var data = new google.visualization.DataView(graphArray_Final); 
+	// Converting Json Object to JavaScript Array
+	for(var j in jsonDataObject){
+			graph_arr.push([jsonDataObject[j].OrderID,jsonDataObject[j].TimeTaken, bar_color[j]]);
+	}
+	var graphArray_Final = google.visualization.arrayToDataTable(graph_arr);
 
-      var options = {
-        title: 'Time Taken for items to be Shipped',
-        hAxis: { title: 'Order ID'},
-        vAxis: { title: 'Time Taken (s)'},
-        legend: { position: "none" },
-      };
-      var chart = new google.visualization.ColumnChart(document.getElementById('column_chart'));
-      chart.draw(data, options);
-    });	 
+	var data = new google.visualization.DataView(graphArray_Final); 
+
+	var options = {
+		title: 'Time Taken for items to be Shipped',
+		hAxis: { title: 'Order ID'},
+		vAxis: { title: 'Time Taken (s)'},
+		legend: { position: "none" },
+	};
+	var chart = new google.visualization.ColumnChart(document.getElementById('column_chart'));
+	chart.draw(data, options);
 }
-
